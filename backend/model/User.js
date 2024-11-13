7d71ced458a8b448086fc33963ed6953397e4834
ramDev <ramprajapati7892@gmail.com>
2024-11-13 12:43:45 +0530

    Update configuration and model for backend

--- backend/model/User.js
+++ backend/model/User.js
@@ -1,22 +1,40 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type : String,
-    required : true,
+  employeename: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'employee', 'manager', "teamlead"],
+    default: 'employee'
   },
   email: {
-    type : String,
-    required : true,
-    unique : true
+    type: String,
+    required: true,
+    unique: true
   },
   password: {
-    type : String,
-    required : true,
+    type: String,
+    required: true,
   },
-  otp : String,
+  avatar: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  otp: String,
   otpExpiration: Date,
-});
+}, { timestamps: true });
 
 const User = mongoose.model("User", UserSchema);
 
